b5067c3c5b10b5b3f0f4dcd3d26c689f85371a8f
snvnn <yunh1205@gmail.com>
2024-12-16 19:44:11 +0900

    import and package update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useAuth } from "../contexts/AuthContext.tsx";
 import { useNavigate } from "react-router-dom";
-import jwtDecode from "jwt-decode"; // JWT 디코딩을 위해 추가
+import { jwtDecode } from "jwt-decode"; // JWT 디코딩을 위해 추가
 import axios from "axios"; // API 호출을 위해 추가
 
 const Login: React.FC = () => {
